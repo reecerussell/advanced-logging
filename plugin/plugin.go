415033550662f9416b47ca7eda546a52868c4638
Reece Russell <me@reece-russell.co.uk>
2020-01-15 17:30:24 +0000

    comments

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -11,10 +11,13 @@ var (
 	m = map[string]Plugin{}
 )
 
+// Plugin is a high-level interface used by the logger to
+// output data through a plugin.
 type Plugin interface {
 	Output(opts map[string]interface{}) io.Writer
 }
 
+// Get returns a plugin from memory.
 func Get(name string) (Plugin, error) {
 	l.RLock()
 	defer l.RUnlock()
@@ -27,6 +30,7 @@ func Get(name string) (Plugin, error) {
 	return plugin, nil
 }
 
+// Register adds a plugin and stores it in memory.
 func Register(name string, plugin Plugin) {
 	l.Lock()
 	defer l.Unlock()
