f6746190104cde5dca39b2b69e4c65fd2e9ed4ca
Reece Russell <me@reece-russell.co.uk>
2020-01-15 17:30:13 +0000

    comments

--- plugin/log/log.go
+++ plugin/log/log.go
@@ -11,14 +11,16 @@ import (
 
 func init() {
 	plugin.Register("log", &Log{
-		internal: gl.New(os.Stderr, "hello", gl.LstdFlags),
+		internal: gl.New(os.Stderr, "", gl.LstdFlags),
 	})
 }
 
+// Log is a plugin for the standard go log package.
 type Log struct {
 	internal *gl.Logger
 }
 
+// Output writes to the Stderr output, with the default log package.
 func (l *Log) Output(opts map[string]interface{}) io.Writer {
 	prefix, ok := opts["prefix"]
 	if ok {
